a73c3abe128c427dc78b7d187b4e8bed13c44b18
Suman Saha <saha1.suman@gmail.com>
2017-06-07 15:48:47 -0700

    Python Script for AWS Alarm and Events added

--- aws-alert/aws-alarm.py
+++ aws-alert/aws-alarm.py
@@ -1,25 +1,31 @@
 import boto3
 
-#Create  Cloudwatch client
+# Create  Cloudwatch client
 cloudwatch = boto3.client('cloudwatch')
 
-#Create Alarm
-cloudwatch.put_metric_alarm(
-    AlarmName='EC2 CPU Utilization',
-    ComparisonOperator='GreaterThanThreshold',
-    EvaluationPeriods=1,
-    MetricName='EC2CPUUtilization',
-    Namespace='AWS/EC2',
-    Period=60,
-    Statistic='Average',
-    Threshold=75.0,
-    ActionsEnabled=False,
-    AlarmDescription='Alarm When Server CPU exceeds 75%',
-    Dimensions=[
-        {
-            'Name': 'InstanceId',
-            'Value': 'INSTANCE_ID'
-        },
-    ],
-    Unit='Seconds'
-)
+INSTANCE_ID = ['i-0496f9e0749f57d01', 'i-02bd4aa0102cfa119']
+
+for instanceid in INSTANCE_ID:
+
+# Create Alarm
+    response = cloudwatch.put_metric_alarm(
+        AlarmName=instanceid + '-EC2 CPU Utilization',
+        ComparisonOperator='GreaterThanThreshold',
+        AlarmActions=[
+            'arn:aws:sns:us-west-2:699325298196:my-topic'],
+        EvaluationPeriods=1,
+        MetricName='CPUUtilization',
+        Namespace='AWS/EC2',
+        Period=300,
+        Statistic='Average',
+        Threshold=75.0,
+        ActionsEnabled=True,
+        AlarmDescription='Alarm When Server CPU exceeds 75%',
+        Dimensions=[
+            {
+                'Name': 'InstanceId',
+                'Value': instanceid
+            },
+        ],
+        Unit='Percent'
+    )
